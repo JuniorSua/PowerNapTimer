0ca555bdd1842e015403af7c05ba81760e3816a0
Junior S <juniorsuarez1014@gmail.com>
2020-11-21 21:30:31 -0500

    Added Functions to the timer Model

--- PowerNapTimer34/PowerNapTimer34/Model/NapTimer.swift
+++ PowerNapTimer34/PowerNapTimer34/Model/NapTimer.swift
@@ -15,4 +15,30 @@ class NapTimer {
         return timeRemaining != nil
     }
     
+    func startTimer(time: TimeInterval) {
+        if !isActive {
+            self.timeRemaining = time
+            self.timeCounter = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (_) in
+                self.secondTicked()
+            })
+        }
+    }
+    
+    private func secondTicked() {
+        guard let timeLeft = timeRemaining else { return }
+        if timeLeft > 0 {
+            self.timeRemaining = timeLeft - 1
+        } else {
+            self.timeRemaining = nil
+            timeCounter?.invalidate()
+            
+        }
+    }
+    
+    func stopTimer() {
+        self.timeRemaining = nil
+        timeCounter?.invalidate()
+        
+    }
+    
 }
